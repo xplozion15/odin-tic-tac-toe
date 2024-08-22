0843e84d12d98678767f4dee989bf726fec8e167
abhilash15500 <89824888+abhilash15500@users.noreply.github.com>
2024-08-22 15:23:17 +0530

    Write js logic for dom manipulation of the array element to web page

--- app.js
+++ app.js
@@ -10,47 +10,43 @@ const playerTwo = createPlayer(playerTwoName, marker = "O");
 let currentPlayer = playerOne;
 let winner = null;
 
-
 function toggleCurrentPlayer() {
     if (currentPlayer == playerOne) {
         currentPlayer = playerTwo;
-    }
-    else if (currentPlayer == playerTwo) {
+    } else if (currentPlayer == playerTwo) {
         currentPlayer = playerOne;
     }
 }
 
-
-
-
 const gameboardObject = (function () {
     const gameboard = ["", "", "", "", "", "", "", "", ""];
+    
+
 
     const getGameBoard = () => gameboard;
 
-
     const updateGameBoard = (index, marker = currentPlayer.marker) => {
-
-
         if (gameboard[index] === "") {
             gameboard[index] = marker;
+           
         }
 
-        toggleCurrentPlayer();
+        
         gameFlow();
+       
+        
+    };
 
-    }
+
+
+
 
     const gameFlow = () => {
-
         const winningConditions = [
             [0, 1, 2], [3, 4, 5], [6, 7, 8],
             [0, 3, 6], [1, 4, 7], [2, 5, 8],
             [0, 4, 8], [2, 4, 6],
         ];
-
-
-
 
         for (let condition of winningConditions) {
             const zeroIndex = condition[0];
@@ -61,37 +57,38 @@ const gameboardObject = (function () {
                 alert(`${playerOne.name} is the winner!`);
                 winner = playerOne;
                 return;
-
-            }
-            else if (gameboard[zeroIndex] == "O" && gameboard[firstIndex] == "O" && gameboard[secondIndex] == "O") {
+            } else if (gameboard[zeroIndex] == "O" && gameboard[firstIndex] == "O" && gameboard[secondIndex] == "O") {
                 alert(`${playerTwo.name} is the winner!`);
                 winner = playerTwo;
                 return;
             }
 
-            
             if (winner === null && !gameboard.includes("")) {
                 alert("Its a draw");
                 winner = null;
                 return;
-
+            }
         }
-   
-    }
 
+        return { gameFlow };
+    };
 
-        return { gameFlow }
-    }
-
-
-
-    return { getGameBoard, updateGameBoard, gameFlow };
+    return { getGameBoard, updateGameBoard, gameFlow};
 })();
 
 
 
 
-
+document.querySelectorAll(".gameboard-grid-cell").forEach(cell => {
+    cell.addEventListener("click", ()=> {      
+        gameboardObject.updateGameBoard(cell.dataset.index);
+        if(cell.textContent != "X" && cell.textContent != "O") {
+            cell.textContent = currentPlayer.marker;
+        }
+       
+        toggleCurrentPlayer();
+    });  
+});
 
 
 
